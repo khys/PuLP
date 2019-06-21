2195fc8c989f0b8eb085a818c1fbf60562449027
Kazuki <guruguru9606@gmail.com>
2019-06-21 18:25:09 +0900

    add variables

--- mano.py
+++ mano.py
@@ -4,8 +4,23 @@ from itertools import product
 from pulp import *
 
 np.random.seed(1)
-nw, nf = 3, 4
-pr = list(product(range(nw), range(nf)))
-供給 = np.random.randint(30, 50, nw)
-需要 = np.random.randint(20, 40, nf)
-輸送費 = np.random.randint(10, 20, (nw, nf))
+ns, nf, nq = 4, 4, 2
+pr = list(product(range(ns),range(ns)))
+
+sCPU = np.random.randint(10, 20, ns)
+fCPU = np.random.randint(0, 10, ns)
+sRAM = np.random.randint(64, 128, ns)
+fRAM = np.random.randint(0, 64, ns)
+
+sBW = np.random.randint(10, 100, (ns, ns))
+sDelay = np.random.randint(10, 100, (ns, ns))
+
+cPrice = np.random.randint(5, 15, ns)
+rPrice = np.random.randint(1, 3, ns)
+bPrice = np.random.randint(2, 2, 1)
+
+cAF = np.random.randint(1, 4, nf)
+rAF = np.random.randint(2, 16, nf)
+
+bChain = np.random.randint(1, 20, nq)
+dChain = np.random.randint(100, 300, nq)
